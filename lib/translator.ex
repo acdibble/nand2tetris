ca277b8d9698bc9d0a970899484bbf9e4e19ff91
Andrew Dibble <andrew@dibble.codes>
2024-06-14 23:09:28 +0200

    handle function calls

--- lib/translator.ex
+++ lib/translator.ex
@@ -49,14 +49,22 @@ defmodule Translator do
     end
 
     defp parse_line("function " <> rest) do
+      [name, locals] = String.split(rest)
+
+      locals = parse_int!(locals)
+
+      {:function, name, locals}
+    end
+
+    defp parse_line("return"), do: :return
+
+    defp parse_line("call " <> rest) do
       [name, arity] = String.split(rest)
 
       arity = parse_int!(arity)
 
-      {:function, name, arity}
+      {:call, name, arity}
     end
-
-    defp parse_line("return"), do: :return
 
     defp get_reg_type(type) do
       case type do
@@ -86,7 +94,7 @@ defmodule Translator do
 
   defmodule Writer do
     defmodule Frame do
-      defstruct name: nil, jumps: 0, enclosing: nil, labels: []
+      defstruct name: nil, jumps: 0, returns: 0, enclosing: nil, labels: [], locals: []
 
       def new(name) when is_binary(name) do
         %Frame{name: name, jumps: 0, enclosing: nil}
@@ -96,25 +104,45 @@ defmodule Translator do
         %Frame{name: name, enclosing: frame}
       end
 
-      def unnest(%Frame{enclosing: enclosing = %Frame{}}) do
-        enclosing
-      end
-
-      def inc_jumps(frame = %Frame{}) do
-        %Frame{frame | jumps: frame.jumps + 1}
+      def unnest(frame = %Frame{}) do
+        case frame.labels do
+          [] -> frame.enclosing
+          _ -> frame
+        end
       end
 
       def jump_labels(frame = %Frame{}) do
         jump_count = Integer.to_string(frame.jumps)
 
         {
-          frame.name <> "if_true_" <> jump_count,
-          frame.name <> "if_done_" <> jump_count,
-          inc_jumps(frame)
+          label(frame, "if_true_" <> jump_count),
+          label(frame, "if_done_" <> jump_count),
+          Map.update!(frame, :jumps, &(&1 + 1))
+        }
+      end
+
+      def return_label(frame = %Frame{}, name) do
+        {
+          label(frame, "return_" <> name <> "_" <> Integer.to_string(frame.returns)),
+          Map.update!(frame, :returns, &(&1 + 1))
         }
       end
 
       def label(frame = %Frame{}, name), do: frame.name <> "$" <> name
+
+      def track_label(frame = %Frame{}, name) do
+        {
+          label(frame, name),
+          %Frame{frame | labels: [name | frame.labels]}
+        }
+      end
+
+      def untrack_label(frame = %Frame{}, name) do
+        {
+          label(frame, name),
+          %Frame{frame | labels: Enum.reject(frame.labels, &(&1 == name))}
+        }
+      end
     end
 
     def write!(stream, static_name) do
@@ -162,28 +190,19 @@ defmodule Translator do
     end
 
     defp write_op({:push, :constant, num}, state) do
-      ops = [
-        "@#{num}",
-        "D=A",
-        "@SP",
-        "A=M",
-        "M=D"
-        | increment_sp()
-      ]
-
-      {ops, state}
+      {push_constant("@#{num}"), state}
     end
 
     defp write_op({:push, :temp, offset}, state) do
-      get_temp_reg(offset) |> push_aliased_register(state)
+      {get_temp_reg(offset) |> push_aliased_register(), state}
     end
 
     defp write_op({:push, :pointer, offset}, state) do
-      get_pointer_reg(offset) |> push_aliased_register(state)
+      {get_pointer_reg(offset) |> push_aliased_register(), state}
     end
 
     defp write_op({:push, :static, offset}, state) do
-      get_static_reg(offset, state) |> push_aliased_register(state)
+      {get_static_reg(offset, state) |> push_aliased_register(), state}
     end
 
     defp write_op({:push, type, offset}, state) do
@@ -282,14 +301,16 @@ defmodule Translator do
     end
 
     defp write_op({:label, label}, state) do
+      {label, frame} = Frame.track_label(state[:frame], label)
+
       {
-        ["(" <> Frame.label(state[:frame], label) <> ")"],
-        state
+        ["(" <> label <> ")"],
+        %{state | frame: frame}
       }
     end
 
     defp write_op({:goto, :jne, label}, state) do
-      label = Frame.label(state[:frame], label)
+      {label, frame} = Frame.untrack_label(state[:frame], label)
 
       ops =
         [
@@ -300,11 +321,11 @@ defmodule Translator do
         ]
         |> decrement_sp()
 
-      {ops, state}
+      {ops, %{state | frame: frame}}
     end
 
     defp write_op({:goto, :jmp, label}, state) do
-      label = Frame.label(state[:frame], label)
+      {label, frame} = Frame.untrack_label(state[:frame], label)
 
       ops =
         [
@@ -312,12 +333,21 @@ defmodule Translator do
           "0;JMP"
         ]
 
-      {ops, state}
+      {ops, %{state | frame: frame}}
     end
 
-    defp write_op({:function, name, _}, state) do
+    defp write_op({:function, name, locals}, state) do
       ops = [
         "(" <> name <> ")"
+        | Enum.take(0..locals, locals)
+          |> Enum.flat_map(fn _ ->
+            [
+              "@SP",
+              "A=M",
+              "M=0"
+              | increment_sp()
+            ]
+          end)
       ]
 
       state = %{state | frame: Frame.nest(state[:frame], name)}
@@ -326,7 +356,7 @@ defmodule Translator do
 
     defp write_op(:return, state) do
       ops = [
-        # save LCL for late
+        # save LCL for later
         "@LCL",
         "D=M",
         "@R13",
@@ -345,7 +375,8 @@ defmodule Translator do
         "A=M",
         "M=D",
         # increment callee's call stack
-        "D=A+1",
+        "@ARG",
+        "D=M+1",
         "@SP",
         "M=D",
         # restore THAT
@@ -390,6 +421,43 @@ defmodule Translator do
       {ops, state}
     end
 
+    defp write_op({:call, name, arity}, state) do
+      {return, frame} = Frame.return_label(state[:frame], name)
+
+      ops =
+        push_constant("@" <> return) ++
+          push_aliased_register("@LCL") ++
+          push_aliased_register("@ARG") ++
+          push_aliased_register("@THIS") ++
+          push_aliased_register("@THAT") ++
+          [
+            "@SP",
+            "D=M",
+            "@LCL",
+            "M=D",
+            "@#{arity + 5}",
+            "D=D-A",
+            "@ARG",
+            "M=D",
+            "@" <> name,
+            "0;JMP",
+            "(" <> return <> ")"
+          ]
+
+      {ops, Map.put(state, :frame, frame)}
+    end
+
+    defp push_constant(value) do
+      [
+        value,
+        "D=A",
+        "@SP",
+        "A=M",
+        "M=D"
+        | increment_sp()
+      ]
+    end
+
     defp increment_sp() do
       [
         "@SP",
@@ -409,8 +477,8 @@ defmodule Translator do
     defp get_temp_reg(offset), do: "@R#{5 + offset}"
     defp get_static_reg(offset, state), do: "@#{state[:static_name]}.#{offset}"
 
-    defp push_aliased_register(reg, state) do
-      ops = [
+    defp push_aliased_register(reg) do
+      [
         reg,
         "D=M",
         "@SP",
@@ -418,8 +486,6 @@ defmodule Translator do
         "M=D"
         | increment_sp()
       ]
-
-      {ops, state}
     end
 
     defp pop_aliased_register(reg, state) do
@@ -443,12 +509,44 @@ defmodule Translator do
         :that -> "@THAT"
       end
     end
+
+    def bootstrap do
+      {ops, _} = write_op({:call, "Sys.init", 0}, %{frame: Frame.new("global")})
+
+      [
+        "@256",
+        "D=A",
+        "@SP",
+        "M=D"
+        | ops
+      ]
+    end
   end
 
-  def translate(file) do
-    [static_name | _] = Path.basename(file) |> String.split(".")
+  def translate(fileOrDir) do
+    case File.dir?(fileOrDir) do
+      true ->
+        {:ok, files} = File.ls(fileOrDir)
 
-    Parser.parse!(file)
-    |> Writer.write!(static_name)
+        files =
+          files
+          |> Enum.filter(&String.ends_with?(&1, ".vm"))
+          |> Enum.map(&Path.join(fileOrDir, &1))
+
+        [Writer.bootstrap()] ++ files
+
+      false ->
+        [fileOrDir]
+    end
+    |> Stream.flat_map(fn
+      list when is_list(list) ->
+        list
+
+      file when is_binary(file) ->
+        [static_name | _] = Path.basename(file) |> String.split(".")
+
+        Parser.parse!(file)
+        |> Writer.write!(static_name)
+    end)
   end
 end
